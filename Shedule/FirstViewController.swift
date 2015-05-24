9be4f27ad365ea10bc96d54cf09447510ad56ec3
Артем Жорницкий <Artem@MacBook-Pro-Artem.local>
2015-05-24 15:25:39 +0500

    Initial Commit

---
+++ Shedule/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Shedule
+//
+//  Created by Артем Жорницкий on 24.05.15.
+//  Copyright (c) 2015 Артем Жорницкий. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Shedule/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Shedule
+//
+//  Created by Артем Жорницкий on 24.05.15.
+//  Copyright (c) 2015 Артем Жорницкий. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
