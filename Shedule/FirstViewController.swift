0c9d59807847396da5c4ee94a81360fa2c083bc8
Евгений <evgenij@ip-192-168-1-105.is74.loc>
2015-05-25 11:54:43 +0500

    Initial Commit

--- Shedule/FirstViewController.swift
+++ Shedule/FirstViewController.swift
@@ -9,8 +9,12 @@
 import UIKit
 
 class FirstViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
-    @IBOutlet weak var Monday: UITableView!
+   // @IBOutlet weak var Table2: UITableView!
     
+    @IBOutlet weak var Table2: UITableView!
+    
+    @IBOutlet weak var Table1: UITableView!
+    //@IBOutlet weak var Table1: UITableView!
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
         
@@ -19,11 +23,21 @@ class FirstViewController: UIViewController,UITableViewDataSource,UITableViewDel
         
     }
     
+    override func viewWillAppear(animated: Bool) {
+        Table1.reloadData()
+        //Table2.reloadData()
+    }
+        
+    
     // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
     // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
     //
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
-        return  //   LessMgr.Lessons
+        
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "Default")
+        cell.textLabel.text = LessMgr.Lessons[indexPath.row].Name
+        cell.detailTextLabel?.text=LessMgr.Lessons[indexPath.row].Teacher;
+        return cell
    }
 
     override func viewDidLoad() {
--- Shedule/SecondViewController.swift
+++ Shedule/SecondViewController.swift
@@ -9,10 +9,21 @@
 import UIKit
 
 class SecondViewController: UIViewController{
+    @IBOutlet weak var LessonName: UITextField!
+    @IBOutlet weak var TeacherName: UITextField!
+    @IBAction func AddButt(sender: AnyObject) {
+        LessMgr.Addlesson(LessonName.text,teacher: TeacherName.text)
+        self.view.endEditing(true)
+        //self.tabBarController?.selectedIndex=0
+        
+    }
 
+   // @IBOutlet weak var PickerView: UIPickerView!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+   
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,14 @@ class SecondViewController: UIViewController{
         // Dispose of any resources that can be recreated.
     }
 
+
+    
+    // info that was fetched and cached from the data source and delegate
+
+
+    
+    // Reloading whole view or single component
+   
 
 }
 
